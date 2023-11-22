759b2711a1d6399562f89890071a0db3c4af37eb
t1xelL <sidorov_timofey05@mail.ru>
2023-11-22 22:50:57 +0300

    add file and test

---
+++ test_circle.py
@@ -0,0 +1,34 @@
+import unittest
+import circle
+
+
+class CircleTestCase(unittest.TestCase):
+    def test_negative_perimetr(self):
+        result = circle.perimeter(-2)
+        self.assertEqual(result, -12.566370614359172)  # "The radius cannot be negative"
+
+    def test_positive_perimeter(self):
+        result = circle.perimeter(5)
+        self.assertAlmostEqual(result, 31.41592653589793)
+
+    def test_zero_perimeter(self):
+        result = circle.perimeter(0)  # Side can not be 0
+        self.assertEqual(result, 0)
+
+    def test_negative_area(self):
+        result = circle.perimeter(-2)
+        self.assertEqual(result, -12.566370614359172)  # "The radius cannot be negative"
+
+    def test_positive_area(self):
+        result = circle.area(5)
+        self.assertAlmostEqual(result, 78.53981633974483)
+
+    def test_zero_area(self):
+        result = circle.area(0)  # Side can not be 0
+        self.assertEqual(result, 0)
+
+
+
+
+
+
---
+++ test_square.py
@@ -0,0 +1,22 @@
+import unittest
+import square
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_perimeter(self):
+        result = square.perimeter(0)  # Side can not be 0
+        self.assertEqual(result, 0)
+
+    def test_positive_perimeter(self):
+        result = square.perimeter(5)
+        self.assertEqual(result, 20)
+
+    def test_zero_area(self):
+        result = square.area(0)  # Side can not be 0
+        self.assertEqual(result, 0)
+
+    def test_positive_area(self):
+        result = square.area(5)
+        self.assertEqual(result, 25)
+
+
+
