7b95abdf3db27c972c3eb81ee813ab53a24f10b3
t1xelL <sidorov_timofey05@mail.ru>
2023-11-23 13:28:28 +0300

    test: update test

--- test_circle.py
+++ test_circle.py
@@ -4,10 +4,12 @@ import circle
 
 class CircleTestCase(unittest.TestCase):
     def test_negative_perimetr(self):
-        result = circle.perimeter(-2)
-        self.assertEqual(result, "Error")  # "The radius cannot be negative"
+        with self.assertRaises(ValueError):
+            result = circle.perimeter(-2) # "The radius cannot be negative"
+
 
     def test_positive_perimeter(self):
+
         result = circle.perimeter(5)
         self.assertAlmostEqual(result, 31.4)
 
@@ -16,12 +18,12 @@ class CircleTestCase(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_negative_area(self):
-        result = circle.perimeter(-2)
-        self.assertEqual(result, "Error")  # "The radius cannot be negative"
+        with self.assertRaises(ValueError):
+            result = circle.perimeter(-2)  # "The radius cannot be negative"
 
     def test_positive_area(self):
         result = circle.area(5)
-        self.assertAlmostEqual(result, 78.53981633974483)
+        self.assertAlmostEqual(result, 78.5)
 
     def test_zero_area(self):
         result = circle.area(0)  # Side can not be 0
